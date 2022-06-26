53c812658721a172ece349d9ccf6173476094a1c
DavidZamoraAcosta <davidzamoracosta@gmail.com>
2022-06-25 19:34:42 -0500

    Servicios

--- BlogLiderar/urls.py
+++ BlogLiderar/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.inicio, name='inicio'),
@@ -10,3 +9,4 @@ urlpatterns = [
     path('contactapp/', include('ContactApp.urls')),
     path('servicios/', include('Servicios.urls')),
 ]
+
--- Servicios/urls.py
+++ Servicios/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from Servicios import views
 
-
 urlpatterns = [
     path('servicios', views.servicios, name='servicios'),
 ]
-
