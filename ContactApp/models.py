34cd79ab014b54727d1dc5e1f8a788467b3b27a2
DavidZamoraAcosta <davidzamoracosta@gmail.com>
2022-06-26 17:18:36 -0500

    Avance Servicios

--- ContactApp/models.py
+++ ContactApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import Textarea
 
 # Create your models here.
 class DudaEstudiante(models.Model):
--- Servicios/urls.py
+++ Servicios/urls.py
@@ -3,4 +3,6 @@ from Servicios import views
 
 urlpatterns = [
     path('servicios', views.servicios, name='servicios'),
+    path('newsletter_form', views.newsletter_form, name='newsletter_form'),
+    path('cursos_form', views.cursos_form, name='cursos_form'),
 ]
