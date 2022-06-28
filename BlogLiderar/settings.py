4bb8302fed8492ab4ca63e8a6a87e7213db23d4a
DavidZamoraAcosta <davidzamoracosta@gmail.com>
2022-06-27 20:11:25 -0500

    Avance Blog

--- BlogLiderar/settings.py
+++ BlogLiderar/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'BlogLiderar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['D:/Python/BlogLiderApp-main/BlogLiderar/templates'],
+        'DIRS': ['G:/My Drive/Estudio/Curso Python/VSC/BlogLiderApp/BlogLiderappF/BlogLiderar/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
