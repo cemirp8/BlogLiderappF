435716f6da0672198986c0a37d6f0e9da6845665
DavidZamoraAcosta <davidzamoracosta@gmail.com>
2022-06-29 17:15:18 -0500

    Avance Usuarios

--- BlogLiderar/settings.py
+++ BlogLiderar/settings.py
@@ -133,5 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
+LOGIN_URL = '/usuarios/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
--- Usuarios/urls.py
+++ Usuarios/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('registro_usuario/', views.registro_usuario, name='registro_usuario'),
+    path('login/', views.login_request, name='login_request'),
+    path('register_request/', views.register_request, name='register_request'),
+    path('logout/', LogoutView.as_view(template_name='Registration/logout.html'), name='logout')
 ]
