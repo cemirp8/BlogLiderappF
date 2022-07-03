73e14a9f589bfe11ab5c48e7497b5b3c043c48f7
DavidZamoraAcosta <davidzamoracosta@gmail.com>
2022-07-03 17:24:49 -0500

    CRUD Usuarios

--- Usuarios/urls.py
+++ Usuarios/urls.py
@@ -6,5 +6,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('login/', views.login_request, name='login_request'),
     path('register_request/', views.register_request, name='register_request'),
-    path('logout/', LogoutView.as_view(template_name='Registration/logout.html'), name='logout')
+    path('logout/', LogoutView.as_view(template_name='Registration/logout.html'), name='logout'),
+    path('edicion_usuario/', views.edicion_usuario, name='edicion_usuario'),
+    
 ]
