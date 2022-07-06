70b88efe06125a4bbd6c5d5216934189c064179b
DavidZamoraAcosta <davidzamoracosta@gmail.com>
2022-07-05 20:34:28 -0500

    FINAL

--- BlogLiderar/settings.py
+++ BlogLiderar/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p6$t+jeqomiq0ofg4e^(phl_kww$!=do*=cpnu&b$*24^rn2@h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost,"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
